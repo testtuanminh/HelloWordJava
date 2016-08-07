cae5ea8ef2b1c65a4cab57c7f59e6146b2b7543b
testtuanminh <test.tuanminh@gmail.com>
2016-08-07 21:27:09 +0700

    test commit has message

---
+++ Test/src/Test/Test.java
@@ -0,0 +1,17 @@
+package Test;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a[] = {1,2,3};
+		for (int i : a) {
+			System.out.println(i);			
+		}
+		
+	}
+
+}
